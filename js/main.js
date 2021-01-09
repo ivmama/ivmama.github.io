ab0ac36baf2964448c35b7eb3819048b77f08174
ivmama <ivan.mazein051001@gmail.com>
2021-01-09 20:43:05 +0200

    init commit

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-// @ts-nocheck
-$(function(){
-  $('.photo__slider').slick({
-      arrows: true,
-      prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
-      nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
-  });
-
-  $('.menu-btn').on('click', function(){
-    $('.menu__list').slideToggle();
-  });
+ let man = document.querySelector('.man');
+    window.addEventListener('mousemove', function(e) {
+        let x = e.clientX / window.innerWidth;
+        let y = e.clientY / window.innerHeight;  
+        man.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
+    });
+    
+    
+    let castle = document.querySelector('.castle');
+    window.addEventListener('mousemove', function(e) {
+        let x = e.clientX / window.innerWidth;
+        let y = e.clientY / window.innerHeight;  
+        castle.style.transform = 'translate(' + x * 20 + 'px, ' + y * 20 + 'px)';
+    });
 
   
-});
+
+
+
+
+
+
