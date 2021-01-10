9ae2b1b80f0897130602af81889a7b4ac4a4a338
Ivan Maziein <58754754+ivmama@users.noreply.github.com>
2021-01-10 11:56:46 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,15 @@
- let man = document.querySelector('.man');
-    window.addEventListener('mousemove', function(e) {
-        let x = e.clientX / window.innerWidth;
-        let y = e.clientY / window.innerHeight;  
-        man.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
-    });
-    
-    
-    let castle = document.querySelector('.castle');
-    window.addEventListener('mousemove', function(e) {
-        let x = e.clientX / window.innerWidth;
-        let y = e.clientY / window.innerHeight;  
-        castle.style.transform = 'translate(' + x * 20 + 'px, ' + y * 20 + 'px)';
-    });
-
-  
-
-
+(function () {
+  window.addEventListener("mousemove", function (e) {
+    if (window.innerWidth > 1000) {
+      const x = e.clientX / window.innerWidth;
+      const y = e.clientY / window.innerHeight;
+      document.querySelector(".castle").style.transform =
+        "translate(" + x * 20 + "px, " + y * 20 + "px)";
+      document.querySelector(".man").style.transform =
+        "translate(-" + x * 50 + "px, -" + y * 50 + "px)";
+    }
+  });
+})();
 
 
 
